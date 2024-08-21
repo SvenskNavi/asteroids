6ca99a4932e1e575f84dd21d27fcffff2844edfd
Jason Johnson <svensk.navi@gmail.com>
2024-08-21 13:45:29 -0500

    Add player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 
 import constants
+from player import Player
 
 
 def main():
@@ -8,6 +9,8 @@ def main():
 
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
 
     dt = 0
 
@@ -17,6 +20,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
